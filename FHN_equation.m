d5c030069aed51ab2acf4b37c168bf7906e16a8f
Ohm80 <omertz@post.bgu.ac.il>
2015-12-28 14:54:24 +0200

    Fixing laplacian

--- FHN_equation.m
+++ FHN_equation.m
@@ -1,6 +1,8 @@
 function [u_t,v_t] = FHN_equation(u,v)
-a0 = 1;
-a1 = 1;
-d  = 100;
-u_t = u - u.^3 - v + laplacian_2D_periodic(u);
-v_t = e.*(u - a1 * v - a0) + d*laplacian_2D_periodic(v);
+a0 = -0.1;
+a1 = 2.0;
+d  = 4.0;
+eps  = 0.05;
+dx = 1.0;
+u_t = u - u.^3 - v + laplacian_2D_periodic(u,dx);
+v_t = eps.*(u - a1 * v - a0) + d*laplacian_2D_periodic(v,dx);
--- laplacian_2D_periodic.m
+++ laplacian_2D_periodic.m
@@ -1,4 +1,4 @@
-function a = laplacian_2D_periodic(a)
+function a = laplacian_2D_periodic(a,dx)
 b = ones(size(a)(1)+2,size(a)(2)+2);
 c = zeros(size(a));
 b(2:end-1,2:end-1)=a;
@@ -9,7 +9,6 @@ b(end,2:end-1)=a(1,:);
 b(1,2:end-1)=a(end,:);
 c = b(2:end-1,3:end)+b(2:end-1,1:end-2)+b(3:end,2:end-1)+b(1:end-2,2:end-1);
 c-= 4.0*b(2:end-1,2:end-1);
-a=c;
-return
+a=c/(dx^2);
 
 
